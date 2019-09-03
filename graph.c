7974c6c067714b8b1f2c37ea496d1217d24f4f49
brenoafb <breno.afb@gmail.com>
2019-09-03 11:04:55 -0300

    Add graph implementation.

---
+++ graph.c
@@ -0,0 +1,82 @@
+#include "graph.h"
+
+void initialize_graph(graph *g, bool directed) {
+  if (!g) return;
+
+  g->nedges = 0;
+  g->nvertices = 0;
+  g->directed = directed;
+
+  for (int i = 0; i < MAXV; i++) {
+    g->edges[i] = NULL;
+    g->degrees[i] = 0;
+  }
+}
+
+void deinit_list(edgenode *e) {
+  if (!e) {
+    edgenode *p = e->next;
+    free(e);
+    deinit_list(p);
+  }
+}
+
+void deinit_graph(graph *g) {
+  if (!g) return;
+  for (int i = 0; i < MAXV; i++) {
+    if (g->edges[i]) {
+      deinit_list(g->edges[i]);
+    }
+    free(g->edges[i]);
+  }
+}
+
+void read_graph(graph *g, bool directed) {
+  if (!g) return;
+  initialize_graph(g, directed);
+
+  int nedges;
+  scanf("%d %d", &g->nvertices, &nedges);
+
+  for (int i = 0; i < nedges; i++) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    insert_edge(g, x, y, directed);
+  }
+}
+
+void insert_edge(graph *g, int x, int y, bool directed) {
+  if (!g) return;
+
+  edgenode *p = malloc(sizeof(edgenode));
+  p->v = y;
+  p->weight = 0;
+  p->next = g->edges[x];
+  g->edges[x] = p;
+  g->degrees[x]++;
+
+  if (!directed) {
+    insert_edge(g, y, x, !directed);
+  } else {
+    g->nedges++;
+  }
+}
+
+void print_list(edgenode *e) {
+  if (!e) {
+    printf("_\n");
+  } else {
+    printf("%d -> ", e->v);
+    print_list(e->next);
+  }
+}
+
+void print_graph(graph *g) {
+  for (int i = 0; i < MAXV; i++) {
+    if (g->edges[i]) {
+      printf("%d -> ", i);
+      print_list(g->edges[i]);
+    }
+  }
+}
+
