46d5ebffd45b678801fdf06e13a7dca5e7a7713e
brenoafb <breno.afb@gmail.com>
2019-09-03 14:31:48 -0300

    Add queue header.

---
+++ queue.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#define MAXQ 1000
+
+typedef struct queue {
+  int a[MAXQ];
+  int n;
+  int start;
+} queue;
+
+void init_queue(queue *q);
+bool is_empty_queue(queue *q);
+void enqueue(queue *q, int x);
+int dequeue(queue *q);
