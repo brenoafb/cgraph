6dd3918486cbc550dbde1ffa8a73f3d6b3161934
brenoafb <breno.afb@gmail.com>
2019-09-03 11:04:46 -0300

    Add graph.h

---
+++ graph.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAXV 1000
+
+typedef struct edgenode {
+  int v;
+  int weight;
+  struct edgenode *next;
+} edgenode;
+
+void deinit_list(edgenode *e);
+void print_list(edgenode *e);
+
+typedef struct graph {
+  edgenode *edges[MAXV];
+  int degrees[MAXV];
+  int nedges;
+  int nvertices;
+  bool directed;
+} graph;
+
+void initialize_graph(graph *g, bool directed);
+void deinit_graph(graph *g);
+void read_graph(graph *g, bool directed);
+void insert_edge(graph *g, int x, int y, bool directed);
+void print_graph(graph *g);
